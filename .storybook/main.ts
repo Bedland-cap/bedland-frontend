e9997313053fcbb89cb1df266ed1ce55b3c97cdf
angbur <ag.grala@gmail.com>
2023-02-08 13:09:38 +0000

    Merged in feature/BL-28-basic-configuration-of-storybook-f (pull request #4)
    BL-28-feat: add basic configuration of storybook
    
    * BL-28-feat: add basic configuration of storybook
    
    
    Approved-by: Marcin Powazka
    Approved-by: Paweł Wolf
    Approved-by: Mateusz Statkiewicz
    Approved-by: Łukasz Kosobucki

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions'
+  ],
+  "features": {
+    interactionsDebugger: true,
+  },
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  },
+  "staticDirs": [
+    "..\\public"
+  ]
+};
+export default config;
