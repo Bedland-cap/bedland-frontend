1e501826eab4cb4e4b7edc39012c08298a57f304
Roksana Rachel <roksana.rachel@capgemini.com>
2023-03-20 10:14:38 +0000

    Merged in feature/BL-164-add-custom-scroll-to-recent-conta (pull request #42)
    Feature/BL-164 add custom scroll to recent conta
    
    * BL-164: Added scroll
    
    BL-164: Changed scroll
    
    BL-164: Deleted bottom space on the scroll
    
    BL-164: Changed page
    
    BL-164: Updated snapshots
    
    BL-164: Changed consts in scroll
    
    
    Approved-by: Mateusz Statkiewicz
    Approved-by: Łukasz Kosobucki

--- src/components/molecules/MessageItem/MessageItem.styled.ts
+++ src/components/molecules/MessageItem/MessageItem.styled.ts
@@ -1,16 +1,25 @@
 import styled from 'styled-components';
 import { PaletteType } from 'theme/theme.types';
 
-export const MessageItemContainer = styled.div<{ palette: PaletteType; active: boolean }>`
-  align-items: center;
-  border-bottom: 1px solid ${({ palette }) => palette.sidebarTabs};
-  display: flex;
-  padding: 0.875rem 1.938rem 0.875rem 1.438rem;
-
+export const HoverContainer = styled.div<{ palette: PaletteType }>`
   :hover {
-    background: ${({ active, palette }) =>
-      active ? `${palette.primaryLight}10` : `${palette.sidebarTabs}10`};
+    background: ${({ palette }) => `${palette.primaryLight}10`};
+  }
+  :last-child {
+    margin-bottom: 0.75rem;
   }
 `;
 
-export default MessageItemContainer;
+export const MessageContainer = styled.div<{ palette: PaletteType }>`
+  align-items: center;
+  border-bottom: 1px solid ${({ palette }) => palette.sidebarTabs};
+  display: flex;
+  margin-left: 1.125rem;
+  margin-right: 1rem;
+  padding: 0.875rem 1.938rem 0.875rem 1.438rem;
+`;
+
+export const NameWrapper = styled.div`
+  flex: 1;
+  margin-left: 1rem;
+`;
