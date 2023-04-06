9edd344009c5599ade43489b466357f40bcea881
Keitar6 <statkprivate@gmail.com>
2023-04-06 06:39:20 +0000

    feat/BL-196: TS checker plugin implemented
    Approved-by: Roksana Rachel
    Approved-by: Łukasz Kosobucki

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import { checker } from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    checker({
+      typescript: true,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './setupTests.ts',
     coverage: {
       reporter: ['text', 'html'],
-      exclude: [
-        './node_modules/',
-        './setupTests.ts'
-      ],
+      exclude: ['./node_modules/', './setupTests.ts'],
     },
-    mockReset: true
-  }
+    mockReset: true,
+  },
 });
