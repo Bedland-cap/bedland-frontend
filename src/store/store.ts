c427ae35e8d7a33e13495a904d9da647fb0e9b6e
LukaszKosobucki <lukasz.kos98@gmail.com>
2023-04-07 12:00:58 +0000

    Merged in feature/BL-116-define-api-and-redux-slice-for-ma (pull request #62)
    Feature/BL-116 define api and redux slice for ma
    
    * BL-116: added endpoint for manager
    
    
    Approved-by: Mateusz Statkiewicz
    Approved-by: Roksana Rachel

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,19 +2,23 @@ import { combineReducers, configureStore, type PreloadedState } from '@reduxjs/t
 import { CurriedGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 import loggerMiddleware from 'redux-logger';
 import { userApi } from 'services/user/userApi';
+import { managerApi } from 'services/manager/managerApi';
 import { residentReducer, buildingReducer, userReducer } from './reducers/index';
+import { managerReducer } from './reducers/manager/manager_slice';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   resident: residentReducer,
+  manager: managerReducer,
   building: buildingReducer,
   [userApi.reducerPath]: userApi.reducer,
+  [managerApi.reducerPath]: managerApi.reducer,
 });
 
 // Adding the api middleware enables caching, invalidation, polling,
 // and other useful features of `rtk-query`.
 export const rootMiddleware = (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
-  getDefaultMiddleware().concat(loggerMiddleware, userApi.middleware);
+  getDefaultMiddleware().concat(loggerMiddleware, userApi.middleware, managerApi.middleware);
 
 export type RootState = ReturnType<typeof rootReducer>;
 
