f3307d89ae49a55f33a305d3a0b04df5bf7aa4ac
Keitar6 <statkprivate@gmail.com>
2023-04-07 12:31:45 +0000

    Feature/BL-114 define api and redux slice for flats
    Approved-by: Roksana Rachel
    Approved-by: Ula Jarych

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,5 +1,6 @@
 import { userReducer } from './user/user_slice';
 import { residentReducer } from './resident/resident_slice';
 import { buildingReducer } from './building/building_slice';
+import { flatReducer } from './flat/flat_slice';
 
-export { buildingReducer, residentReducer, userReducer };
+export { buildingReducer, residentReducer, userReducer, flatReducer };
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,8 +2,9 @@ import { combineReducers, configureStore, type PreloadedState } from '@reduxjs/t
 import { CurriedGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 import loggerMiddleware from 'redux-logger';
 import { userApi } from 'services/user/userApi';
+import { flatApi } from 'services/flat/flatApi';
 import { managerApi } from 'services/manager/managerApi';
-import { residentReducer, buildingReducer, userReducer } from './reducers/index';
+import { residentReducer, buildingReducer, userReducer, flatReducer } from './reducers/index';
 import { managerReducer } from './reducers/manager/manager_slice';
 
 export const rootReducer = combineReducers({
@@ -11,14 +12,16 @@ export const rootReducer = combineReducers({
   resident: residentReducer,
   manager: managerReducer,
   building: buildingReducer,
+  flat: flatReducer,
   [userApi.reducerPath]: userApi.reducer,
+  [flatApi.reducerPath]: flatApi.reducer,
   [managerApi.reducerPath]: managerApi.reducer,
 });
 
 // Adding the api middleware enables caching, invalidation, polling,
 // and other useful features of `rtk-query`.
 export const rootMiddleware = (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
-  getDefaultMiddleware().concat(loggerMiddleware, userApi.middleware, managerApi.middleware);
+  getDefaultMiddleware().concat(loggerMiddleware, userApi.middleware, flatApi.middleware, managerApi.middleware);
 
 export type RootState = ReturnType<typeof rootReducer>;
 
