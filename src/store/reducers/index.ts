72d08d0272d9fd8fb9d030b3f4a39c4a20351074
ujarych <urszula.jarych@gmail.com>
2023-04-13 07:50:07 +0000

    Merged in feature/BL-115-define-api-and-redux-slice-for-voting (pull request #67)
    Feature/BL-115 define api and redux slice for voting
    
    * Merge branch 'develop' into feature/BL-115-define-api-and-redux-slice-for-voting
    
    * feat/BL-115: added votings slice
    
    * Merge branch 'develop' into feature/BL-115
    
    * feat/BL-115: voting widget test update
    
    * Merge branch 'develop' into feature/BL-115-define-api-and-redux-slice-for-voting
    
    * feat/BL-115: comments removed
    
    * feat/BL-115: merge develop
    
    * feat/BL-115: test updated
    
    * Merge branch 'develop' into feature/BL-115-define-api-and-redux-slice-for-voting
    
    * feat/BL-115: tests updated
    
    
    Approved-by: Mateusz Statkiewicz
    Approved-by: Roksana Rachel

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -2,5 +2,6 @@ import { userReducer } from './user/user_slice';
 import { residentReducer } from './resident/resident_slice';
 import { buildingReducer } from './building/building_slice';
 import { flatReducer } from './flat/flat_slice';
+import { votingsReducer } from './votings/votings_slice';
 
-export { buildingReducer, residentReducer, userReducer, flatReducer };
+export { buildingReducer, residentReducer, userReducer, flatReducer, votingsReducer };
